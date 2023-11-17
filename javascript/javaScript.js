bc3bb2035641433eb34462c21080c7ed6d060f97
sandridani <sandri.dani.g85@gmail.com>
2023-11-17 18:00:18 -0300

    ingrese las propiedades destacadas

---
+++ javascript/javaScript.js
@@ -0,0 +1,8 @@
+// Función que se ejecutará al hacer clic en el botón
+function handleClick() {
+    alert('¡Has hecho clic en el botón!');
+    // Otra lógica que quieras ejecutar al hacer clic
+  }
+  
+  // Asignar la función al evento de clic del botón
+  document.getElementById('info0001').addEventListener('click', handleClick);
