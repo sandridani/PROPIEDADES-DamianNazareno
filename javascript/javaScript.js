deb1c16c0a660bf08b21601e02e9ab850656554b
sandridani <sandri.dani.g85@gmail.com>
2023-12-04 17:41:21 -0300

    hice un cambio genial con enlaces

--- javascript/javaScript.js
+++ javascript/javaScript.js
@@ -1,8 +1,22 @@
-// Función que se ejecutará al hacer clic en el botón
-function handleClick() {
-    alert('¡Has hecho clic en el botón!');
-    // Otra lógica que quieras ejecutar al hacer clic
-  }
-  
-  // Asignar la función al evento de clic del botón
-  document.getElementById('info0001').addEventListener('click', handleClick);
+// Configura EmailJS con tu usuario y plantilla de correo electrónico
+emailjs.init("user_tuCodigoDeUsuario");
+
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita el envío estándar del formulario
+
+  // Obtiene los valores del formulario
+  var formData = new FormData(event.target);
+  var data = {};
+  formData.forEach(function(value, key){
+      data[key] = value;
+  });
+
+  // Envía el formulario a través de EmailJS
+  emailjs.send("service_tuCodigoDeServicio", "template_tuCodigoDePlantilla", data)
+    .then(function(response) {
+      console.log("Correo electrónico enviado con éxito:", response);
+    }, function(error) {
+      console.log("Error al enviar el correo electrónico:", error);
+    });
+});
+
